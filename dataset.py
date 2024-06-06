b4573b7890d46f6b22d64c7fc8d956cbc68830d3
ironhanhan01 <wsh23968@gmail.com>
2024-06-06 20:27:17 +0800

    fouth

--- dataset.py
+++ dataset.py
@@ -107,7 +107,9 @@ class InferenceSetLoader(Dataset):
     def __init__(self, dataset_dir, train_dataset_name, test_dataset_name, img_norm_cfg=None):
         super(InferenceSetLoader).__init__()
         self.dataset_dir = dataset_dir + '/' + test_dataset_name
-        with open(self.dataset_dir + '/img_idx/test' + '.txt', 'r') as f:
+        # with open(self.dataset_dir + '/img_idx/test' + '.txt', 'r') as f:
+        with open(self.dataset_dir + '/img_idx/test_' + test_dataset_name + '.txt', 'r') as f:
+
             self.test_list = f.read().splitlines()
         if img_norm_cfg == None:
             self.img_norm_cfg = get_img_norm_cfg(train_dataset_name, dataset_dir)
